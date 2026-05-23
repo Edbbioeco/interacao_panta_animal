7f9eed9351c51caf34499296f639938733cb246e
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-22 23:38:09 -0300

    Criar reposirório do GitHub

---
+++ settar_github.R
@@ -0,0 +1,15 @@
+# Pacotes ----
+
+library(usethis)
+
+# Iniciar sessão ----
+
+usethis::use_git()
+
+# Gerar repositório ----
+
+usethis::use_github()
+
+# Criar README ----
+
+usethis::use_readme_md()
