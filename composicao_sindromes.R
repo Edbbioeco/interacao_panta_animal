8996b93502ed8f5274f010d8c9a2aedec6c2e419
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-23 00:15:41 -0300

    Importar e visualizar os dados

--- composicao_sindromes.R
+++ composicao_sindromes.R
@@ -11,3 +11,16 @@ library(ggview)
 library(betapart)
 
 library(betapart)
+
+# Dados ----
+
+## Importar ----
+
+comp <- readxl::read_xlsx("Base de dados dispersão.xlsx",
+                          sheet = 2)
+
+### Visualizar ----
+
+comp
+
+comp |> dplyr::glimpse()
