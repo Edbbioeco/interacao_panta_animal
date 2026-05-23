8b7a4280993c76eb42edb389ad62a562b3e170c7
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-22 23:37:43 -0300

    Script para os comandos de Git

---
+++ git_comandos.R
@@ -0,0 +1,30 @@
+# Pacootes ----
+
+library(gert)
+
+# Arquivos áptos ----
+
+gert::git_status() |>
+  as.data.frame()
+
+# Adicionar arquivos ----
+
+gert::git_add(files = "git_comandos.R")
+
+# Commit ----
+
+gert::git_commit(message = "Script para os comandos de Git")
+
+# Push ----
+
+gert::git_push(remote = "origin")
+
+# Pull ----
+
+gert::git_pull(remote = "origin")
+
+# Reset ----
+
+gert::git_reset_mixed()
+
+gert::git_reset_soft(ref = "HEAD^1")
